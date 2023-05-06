af3a261a8e0add2fb9f34fe0e27e958cb7625c51
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-05-06 20:57:52 +0800

    Update Array_Sort2.cpp

--- Array_Sort2.cpp
+++ Array_Sort2.cpp
@@ -1,102 +1,149 @@
-#include<iostream>
-#include<iomanip>
-#include<vector>
+#include <iostream>
 using namespace std;
+void debug_displaylist();
+void displaylist();
+void copylist1();
+int identifyNumber(int number_list[], int position);
 
-int number_list1[50];
-int number_list2[50];
+int size;
+int number_list1[20];
+int number_list2[20];
+int compare_list[20];
 
-int main()
-{
-	int choice, element, element_new, position;
-	int flag_list=0;
-	int i=0, maxsize=1;
+
+int main(){
+
+	//three declarations used in the ordering
+	//current for current number to compare. Adjust to adjust and move the current number up.
+	int current=0;
+	int adjust=0;
+	int run=0;
+	int position;
 	
-	while(flag_list==0){
-	for(i=0; i<maxsize; i++){
-		cout << "Enter number: ";
-		cin >> element;
+	cout << "\nEnter a non-integer or letter to end inputs.\n\n";
+	for(int i=0; i<=size; i++){
+		
+		cout << "(Max 20) Enter number for element " << size << ": ";
+		cin >> number_list1[i];
+		size++;
 	
 		if(cin.fail()){
-			i=maxsize+1;
 			cin.clear();
 			cin.ignore(256, '\n');
-			maxsize--;
-			flag_list=1;
+			size--;
+			}
+		else if(size==21){
+			size--;
+		}
+			
+	}
+	
+	copylist1(); //copies number_list1 to the other two lists for referencing.
+	
+	cout << "\nLists: (Unmodified)\n\n";
+	displaylist();
+	
+	//debug, confirm size.
+	//cout << "\n\n\nSize: " << size << "\n";
+	
+	for(int current=0; current<size; current++){
+		
+		for(int swap=current; swap<size; swap++){
+
+			if(number_list1[current] >= number_list2[swap]){
+				
+				number_list1[current] = number_list2[swap]; 
+				number_list1[swap] = number_list2[current]; 
+				
+				copylist1(); //copies the new, modified list to both 
+				}
+			
+		}
+		
+		
+		//debug
+		//cout << debug_displaylist();
+		//cout << "\nCurrent is: " << current;
+		//cout << "\nAdjust is: " << adjust;
+		
+	}
+	
+	cout << "\n\nLists: (Corrected)\n\n";
+
+	displaylist();
+	system("PAUSE");
+	system("CLS");
+
+	while(run==0){
+		
+		displaylist();
+		cout << "\nCurrent Size of Array: " << size << " [0 to " << size-1 << "] \n\n" ; 
+		cout << "\n\nPinpoint an element's position to identify [Input non-integer to exit]: ";
+		cin >>	position;
+		
+		
+		if( position>=0 && position<=(size-1) ){
+			cout << "\nNumber in position " << position << " is: " << identifyNumber(number_list1, position) << "\n\n";
+			system("PAUSE");
+			system("CLS");
+			}
+			
+		else if(cin.fail()){
+			cin.clear();
+			cin.ignore(256, '\n');
+			run=1;
 			}
 		else{
-			number_list1[i]=element;
-			number_list2[i]=element;
-			maxsize++;
-			}
+			cin.ignore(256, '\n');
+			cout << "\nWrong index/position. Please reenter.\n";
+			system("PAUSE");
+			system("CLS");
+		}		
 		
-		}
 	}
-	
-	
-	
-	while(flag_list==1){
-	cout << "\n\n";
-	for(i=0; i<maxsize; i++){
-		cout << number_list1[i] << " ";
-		number_list2[i]=number_list1[i]; //This line is to renew the second/"backup" list with the new updated list that includes new numbers and replacements.
-		}
+	return 0;
+}
 
-	cout << "\n\nOptions.";
-	cout << "\n1. Insert new number.";
-	cout << "\n2. Replace number.";
-	cout << "\n3. Exit.\n";
-	cout << "Choice: ";
-	cin >> choice;
+
+void displaylist(){
 	
-	switch(choice)
-	{
-		
-		case 1:
-		{
-		cout << "Enter new number: ";
-		cin >> element_new;
-		cout << "Enter position in the list: ";
-		cin >> position;
-		position=position-1;
-		maxsize++;
-		number_list1[position]=element_new;
-		
-			for(position; position<maxsize; position++){
-				number_list1[(position+1)]=number_list2[position];
-				}
-				
-		//DEBUG: cout << "\n" << position;
-		//Learned: position's value in the for loop, since it was not declared as a new variable within the for loop itself, will carry over. Debug code above confirms it, it will result into maxsize.
-		//As a result, and when position is called for again, its new value will be whatever the final value that [position] has at the end of the for loop.
-		//Solution: number_list1[position]=element should be called first before the for loop above.
-		//number_list1[position]=element_new;
-		//Solution 2: declare a new iterative. Example: for(int i=position; i<maxsize; i++) instead of using position.
-		
-		break;
-		}	
-		
-		case 2:
-		{
-		cout << "Enter new number: ";
-		cin >> element_new;
-		cout << "Enter position in the list: ";
-		cin >> position;
-		position=position-1;
-		number_list1[position]=element_new;
-		break;
-		}
-		
-		
-		case 3:
-		{
-		
-		return 0;
-		}
+	cout << "List of numbers: ";
+	for(int i=0; i<size; i++){
+		cout << number_list1[i] << " ";	
 	}
 	
 	
+}
+
+void debug_displaylist(){
 	
+	cout << "List 1: ";
+	for(int i=0; i<size; i++){
+		cout << number_list1[i] << " ";	
 	}
 	
+	cout << "\n\nList 2: ";
+	for(int i=0; i<size; i++){
+		cout << number_list2[i] << " ";	
+	}
+	
+	cout << "\n\nCompare List: ";
+	for(int i=0; i<size; i++){
+		cout << compare_list[i] << " ";	
+	}
+	
+}
+
+
+
+void copylist1(){
+	for(int i=0; i<size; i++){
+		number_list2[i]=number_list1[i];
+		compare_list[i]=number_list1[i];	
+	}
+	
+}
+
+int identifyNumber(int number_list1[], int position){
+	return number_list1[position];
 }
